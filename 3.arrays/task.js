468c0460e35f7a39428c7229569f221a843c5ae7
Alex <zazerny@gmail.com>
2023-07-20 11:29:30 +0300

    Task Solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
-function compareArrays(arr1, arr2) {
-  
+function compareArrays(arr1, arr2) { 
+    let result;
+	result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+	return result;   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if(users.length === 0){
+        return 0;
+    } else if (gender === "мужской" || gender === "женский"){
+        return users.filter(item => item.gender === gender).reduce((acc, item, idx, arr) => {
+        acc += item.age;
+        if (idx === arr.length - 1){
+            return acc/ arr.length;
+        } else {
+            return acc;
+        }
+  }, 0);} else{
+            return 0;
+        }
+
 }
